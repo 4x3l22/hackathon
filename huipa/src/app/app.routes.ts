2031c829570aae3e137f957246c9a0724423189f
JsanchezCorp <juansanchezcorporativo@gmail.com>
2025-11-07 13:32:45 -0500

    vista recuperación de contraseña

--- huipa/src/app/app.routes.ts
+++ huipa/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register';
 import { Login } from './pages/login/login';
+import { Resetpassword } from './pages/resetpassword/resetpassword';
 
 export const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent,
     },
-    { path: 'login', component: Login}
+    { 
+        path: 'login', 
+        component: Login
+    },
+    {
+        path: 'reset-password',
+        component: Resetpassword
+    }
 ];
