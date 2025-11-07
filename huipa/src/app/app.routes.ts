0881adc014104dcc521c7904c1e822cbeeca6df8
daisy-bait <kadanarpa@gmail.com>
2025-11-07 15:05:33 -0500

    feat: add home page

--- huipa/src/app/app.routes.ts
+++ huipa/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register';
 import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
+    {
+        path: 'home',
+        component: Home
+    },
     {
         path: 'register',
         component: RegisterComponent,
     },
-    { path: 'login', component: Login}
+    { 
+        path: 'login',
+        component: Login
+    },
 ];
